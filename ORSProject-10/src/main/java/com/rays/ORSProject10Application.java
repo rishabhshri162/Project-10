1047d01ccceffdf9b056576886fa2c4041465fa7
Rishabh Shrivastava <rishabhshrivastava7565@gmail.com>
2026-03-09 15:25:09 +0530

    done

---
+++ ORSProject-10/src/main/java/com/rays/ORSProject10Application.java
@@ -0,0 +1,17 @@
+package com.rays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ORSProject10Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ORSProject10Application.class, args);
+
+		System.out.println("Spring boot application successfully started!...");
+
+	}
+
+}
